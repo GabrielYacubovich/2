2dd4743159b6588dacdb3ac1fad96772f0d832b3
GabrielYacubovich <51945316+GabrielYacubovich@users.noreply.github.com>
2023-04-26 18:34:06 +0300

    Update game.js

--- game.js
+++ game.js
@@ -58,8 +58,6 @@ window.addEventListener('resize', () => {
   let asteroids = [];
   let enemies = [];
   let powerups = [];
-  let Bullet;
-
   let enemyBullets = []; 
   let asteroidTimer = 100;
   let enemyTimer = 400;
@@ -85,7 +83,42 @@ function getRandomSpaceFact() {
   function togglePause() {
     paused = !paused;
   }
-
+  class Bullet {
+    constructor(x, y, rotation, doubleShooting, isPlayerBullet = true, type = "", bulletSpeed = 10) {
+      this.x = x;
+      this.y = y;
+      this.rotation = rotation;
+      this.size = 5;
+      this.speed = 0;
+      this.speed = bulletSpeed;
+      this.doubleShooting = doubleShooting;
+      this.isPlayerBullet = isPlayerBullet;
+      this.type = type; 
+    }
+    update() {
+      this.x += this.speed * Math.sin(this.rotation);
+      this.y -= this.speed * Math.cos(this.rotation);
+    }
+    draw(ctx) {
+      ctx.save();
+      ctx.translate(this.x, this.y);
+      ctx.rotate(this.rotation);
+      ctx.beginPath();
+      ctx.arc(0, 0, this.size / 2, 0, Math.PI * 2);
+      ctx.closePath();
+      ctx.fillStyle = this.isPlayerBullet ? "white" : "red"; 
+      ctx.fill();
+      ctx.restore();
+    }
+    offscreen() {
+      return (
+        this.x < 0 ||
+        this.x > canvas.width ||
+        this.y < 0 ||
+        this.y > canvas.height
+      );
+    }
+  }
   function incrementScore(points) {
     const oldScoreMultiple = Math.floor(score / 1000);
     score += points;
